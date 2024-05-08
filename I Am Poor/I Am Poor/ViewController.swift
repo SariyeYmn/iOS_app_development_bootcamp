7a4fdbbe6153b2c6bef5ea5fb3eb0960ac2f99d6
SariyeYaman <101094825+SariyeYmn@users.noreply.github.com>
2024-05-08 19:14:23 +0300

    commit

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Sariye Yaman on 5.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
